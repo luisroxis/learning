1ba704a355460f91ee267971282aa1f72ba20496
Luis Carlos <roxisxprol@gmail.com>
2022-06-01 22:10:06 -0300

    Add Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './view/header/header.component';
+import { PanelComponent } from './view/panel/panel.component';
+import { ProgressComponent } from './view/panel/progress/progress.component';
+import { AttemptComponent } from './view/panel/attempt/attempt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PanelComponent,
+    ProgressComponent,
+    AttemptComponent
   ],
   imports: [
     BrowserModule,
